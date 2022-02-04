01104b98e4ffc3749a29c588311970a247729185
Kelly Lee <amorfati38@gmail.com>
2022-02-03 20:25:40 -0700

    display projects

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
+const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
 
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//     if (err) throw err; 
-
-//     console.log ('Portfolio complete! Check out index.html to see the output!')
-// });
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -147,5 +141,17 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData)
+        const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('index.html', generatePage(portfolioData), err => {
+    if (err) throw err; 
+    console.log ('Portfolio complete! Check out index.html to see the output!')
     });
+    });
+
+
+const mockData = {
+    name: 'Lernantino',
+    github: 'lernantino',
+    projects: []
+    }
